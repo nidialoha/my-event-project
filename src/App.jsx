7f8e906e28491809fc4953b0dc2f195bad81cd55
nidialoha <nidialoha@gmail.com>
2025-03-14 11:39:22 +0100

    function signup, protected Route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import MainLayout from "./Layout/MainLayout";
 import Home from "./Pages/Home";
 import NotFound from "./Pages/NotFound";
+import Signup from "./Pages/Signup";
 
 function App() {
   return (
@@ -9,6 +10,9 @@ function App() {
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route index element={<Home />} />
+
+          {/* //Nested Rootes */}
+          <Route path="sign-up" element={<Signup />} />
 
           {/* NOT FOUND PAGE */}
           <Route path="*" element={<NotFound />} />
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,87 @@
-import React from "react";
-
 function Home() {
   return (
-    <div>
-      <h1>HOME</h1>
-    </div>
+    <>
+      <h1 className="m-6 font-black">Events</h1>
+      <div className="grid grid-cols-3 gap-2">
+        <div className="card bg-base-100 w-96 shadow-sm m-6">
+          <figure>
+            <img
+              src="https://cdn.pixabay.com/photo/2019/03/12/20/29/music-4051779_1280.jpg"
+              alt="konzert1"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">
+              Card Title
+              <div className="badge badge-secondary">NEW</div>
+            </h2>
+            <div>
+              <p>Ort, Datum 2025</p>
+            </div>
+            <p>
+              A card component has a figure, a body part, and inside body there
+              are title and actions parts
+            </p>
+            <div className="card-actions justify-end">
+              <div className="badge badge-outline">Rock</div>
+              <div className="badge badge-outline">Classic</div>
+            </div>
+          </div>
+        </div>
+
+        <div className="card bg-base-100 w-96 shadow-sm m-6">
+          <figure>
+            <img
+              src="https://cdn.pixabay.com/photo/2019/03/12/20/29/music-4051779_1280.jpg"
+              alt="konzert1"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">
+              Card Title
+              <div className="badge badge-secondary">NEW</div>
+            </h2>
+            <div>
+              <p>Ort, Datum 2025</p>
+            </div>
+            <p>
+              A card component has a figure, a body part, and inside body there
+              are title and actions parts
+            </p>
+            <div className="card-actions justify-end">
+              <div className="badge badge-outline">Rock</div>
+              <div className="badge badge-outline">Classic</div>
+            </div>
+          </div>
+        </div>
+
+        <div className="card bg-base-100 w-96 shadow-sm m-6">
+          <figure>
+            <img
+              src="https://cdn.pixabay.com/photo/2019/03/12/20/29/music-4051779_1280.jpg"
+              alt="konzert1"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">
+              Card Title
+              <div className="badge badge-secondary">NEW</div>
+            </h2>
+            <div>
+              <p>Ort, Datum 2025</p>
+            </div>
+            <p>
+              A card component has a figure, a body part, and inside body there
+              are title and actions parts
+            </p>
+            <div className="card-actions justify-end">
+              <div className="badge badge-outline">Rock</div>
+              <div className="badge badge-outline">Classic</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
